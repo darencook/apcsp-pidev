196d6fdcdd8e3e094fa1bff281512730d8ba3a93
Daren <cookd1304@my.sduhsd.net>
2020-01-10 13:42:32 -0800

    change

--- mfile/student.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "student.h"
-
-void printStudent(struct Student* student)
-{
-  printf("Student: %s %s\n", student->fname, student->lname);
-  printf("Age: %d\n", student->age);
-  printf("ID: %d\n", student->studentID);
-}
--- mfile/student.h
+++
@@ -1,8 +0,0 @@
-struct Student {
-  char fname[50];
-  char lname[50];
-  int age;
-  int studentID;
-};
-
-void printStudent();
